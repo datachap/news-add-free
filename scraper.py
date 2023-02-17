fcf74279969f8123c36e02521a8baa403fbdf9e6
iameugn <metajjuxhin@gmail.com>
2023-02-18 00:04:55 +0100

    It is able to get title and the main text

--- scraper.py
+++ scraper.py
@@ -1,12 +1,13 @@
-import requests
 from bs4 import BeautifulSoup
 import time
+import requests
 
 # URL of the website to extract text from
 url = 'https://fortune.com/2023/02/17/bao-fan-missing-china-renaissance-shares/'
 
 # Set the user agent
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+
 
 
 # Send a GET request to the URL with a delay of 1 second between requests
@@ -19,8 +20,11 @@ while True:
 # Use BeautifulSoup to parse the HTML content of the response
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Extract the text from the HTML content
-text = soup.get_text()
+# Find the first <p> tag in the HTML content
+p_title = soup.find("h1")
+p_paragraphs = soup.find('p')
+# Get all the text content from the <p> tag
+p_text = p_title.get_text() + p_paragraphs.get_text()
 
-# Print the extracted text
-print(text)
+# Print the text content
+print(p_text)
