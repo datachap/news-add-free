1ccd4d48307aaea90aa5fcf6c8e17ae4252e68ed
iameugn <metajjuxhin@gmail.com>
2023-02-19 23:24:52 +0100

    Added news fetcher to get headlines url.

---
+++ news_fetcher.py
@@ -0,0 +1,25 @@
+# Using the News API to fetch news headlines
+
+import requests
+
+def getNewsHeadlinesUrl():
+    api_key = "67848e91644e4e8d9dce4e8bba97f66d"
+    url = "https://newsapi.org/v2/top-headlines"
+    params = {
+        "apiKey": api_key,
+        "category": "general",
+        "language": "en",
+        "pageSize": 50
+    }
+
+    # make the API request
+    response = requests.get(url, params=params)
+
+    # parse the JSON response
+    urlList = []
+    data = response.json()
+
+    # extract the article titles
+    for article in data["articles"]:
+        urlList.append(article["url"])
+    return urlList
--- scraper.py
+++ scraper.py
@@ -1,14 +1,13 @@
 from bs4 import BeautifulSoup
 import time
 import requests
+from news_fetcher import getNewsHeadlinesUrl
 
 # URL of the website to extract text from
-url = 'https://fortune.com/2023/02/17/bao-fan-missing-china-renaissance-shares/'
+url = getNewsHeadlinesUrl()
 
 # Set the user agent
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-
-
 
 # Send a GET request to the URL with a delay of 1 second between requests
 while True:
@@ -20,11 +19,16 @@ while True:
 # Use BeautifulSoup to parse the HTML content of the response
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Find the first <p> tag in the HTML content
-p_title = soup.find("h1")
+# Find the tags, which are the most relevant, in the HTML content
+p_h1 = soup.find("h1")
+p_title = soup.find("title")
 p_paragraphs = soup.find('p')
+
 # Get all the text content from the <p> tag
-p_text = p_title.get_text() + p_paragraphs.get_text()
+if p_h1 is p_title:
+    p_text = p_title.get_text() + p_paragraphs.get_text()
+elif p_h1 is not p_title and p_h1 is not None and p_title is not None:
+    p_text = p_h1.get_text() + p_title.get_text() + p_paragraphs.get_text()
 
 # Print the text content
 print(p_text)
