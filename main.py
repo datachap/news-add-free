e770d254509cc1d3e36a1a3fc1d992ec7844a371
iameugn <metajjuxhin@gmail.com>
2023-10-05 22:02:27 +0200

    Changes for the proccesses

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from newsFetcher import get_news_headlines
 from newsScraper import getNewsInfo
 from newsSummarizer import generate_html
@@ -8,10 +10,22 @@ def main():
     
     html_content = generate_html(getNewsInfo(headlines))
 
-    # Save the HTML content to a file (you can also serve it using a web server)
+    # Save the HTML content to a file
     with open("news.html", "w", encoding="utf-8") as html_file:
         html_file.write(html_content)
 
+    # Commit and push changes to GitHub
+    commit_and_push_changes()
+
+def commit_and_push_changes():
+    # Add news.html to Git staging area
+    subprocess.run(["git", "add", "news.html"])
+    
+    # Commit the changes with a message
+    subprocess.run(["git", "commit", "-m", "Update news.html"])
+
+    # Push the changes to GitHub
+    subprocess.run(["git", "push"])
+
 if __name__ == "__main__":
     main()
-    
