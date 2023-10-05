e5ed9947ce79cc2ebde4ac6177e4883c545d486c
iameugn <metajjuxhin@gmail.com>
2023-10-05 22:42:54 +0200

    Actions

--- main.py
+++ main.py
@@ -14,18 +14,6 @@ def main():
     with open("news.html", "w", encoding="utf-8") as html_file:
         html_file.write(html_content)
 
-    # Commit and push changes to GitHub
-    commit_and_push_changes()
-
-def commit_and_push_changes():
-    # Add news.html to Git staging area
-    subprocess.run(["git", "add", "news.html"])
-    
-    # Commit the changes with a message
-    subprocess.run(["git", "commit", "-m", "Update news.html"])
-
-    # Push the changes to GitHub
-    subprocess.run(["git", "push"])
 
 if __name__ == "__main__":
     main()
